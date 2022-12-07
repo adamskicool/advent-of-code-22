c9be8a7da8de8f03ebbd82bdfe500c5450f15476
adamjoha <adamtorkkeli@gmail.com>
2022-12-07 14:20:51 +0100

    day-7-puzzle-2

--- day-7/index.ts
+++ day-7/index.ts
@@ -118,9 +118,41 @@ async function puzzleOne() {
 			.reduce((sum: number, curr: number) => sum + curr, 0);
 	}
 
-	console.log(sumDirectorySizes(directoryMap, 100000));
+	console.log('Puzzle 1:', sumDirectorySizes(directoryMap, 100000));
 }
-async function puzzleTwo() {}
+async function puzzleTwo() {
+	const directoryMap: DirectoryMap = await parseDirectories();
+
+	const totalMemory: number = 70000000;
+	const requiredUnusedMemory: number = 30000000;
+	const usedMemory: number = calculateDirectorySize(directoryMap['/']);
+	const freeMemory: number = totalMemory - usedMemory;
+	const memoryToFree: number = requiredUnusedMemory - freeMemory;
+
+	function getAllDirectorySizes(directory: Directory): number[] {
+		const currentDirectorySize: number = calculateDirectorySize(directory);
+
+		const subDirectorySizes: number[] = Object.values(
+			directory.subDirectories
+		).reduce(
+			(acc: number[], directory: Directory) => [
+				...acc,
+				...getAllDirectorySizes(directory),
+			],
+			[]
+		);
+
+		return [currentDirectorySize, ...subDirectorySizes];
+	}
+
+	const directorySizes: number[] = getAllDirectorySizes(directoryMap['/']);
+
+	const size = directorySizes
+		.sort((a: number, b: number) => (a < b ? -1 : 1))
+		.find((size: number) => size > memoryToFree);
+
+	console.log('Puzzle 2:', size);
+}
 
 puzzleOne();
 puzzleTwo();
