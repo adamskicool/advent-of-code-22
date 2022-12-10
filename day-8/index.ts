31797cc69f1da3ad79896f75a1ef3a6b17ea3f38
adamjoha <adamtorkkeli@gmail.com>
2022-12-10 11:14:04 +0100

    day-8-puzzle-2

--- day-8/index.ts
+++ day-8/index.ts
@@ -2,6 +2,12 @@ import * as fs from 'fs';
 import * as readline from 'readline';
 
 type TreeHeightGrid = number[][];
+enum Direction {
+	Left = 'left',
+	Right = 'right',
+	Up = 'up',
+	Down = 'down',
+}
 
 async function parseTreeHeightGrid(): Promise<TreeHeightGrid> {
 	const fileStream = fs.createReadStream(`${__dirname}/input.txt`);
@@ -44,6 +50,52 @@ function checkVisibilityGridSubset(
 	}
 
 	return true;
+}
+
+function getVisibility(
+	treeHeightGrid: TreeHeightGrid,
+	currentRow: number,
+	currentColumn: number,
+	direction: Direction
+): number {
+	const numRows: number = treeHeightGrid.length;
+	const numColumns: number = treeHeightGrid[0].length;
+	const currentTreeHeight: number = treeHeightGrid[currentRow][currentColumn];
+	let count: number = 0;
+	if (direction === Direction.Left) {
+		for (let column = currentColumn - 1; column >= 0; column--) {
+			count++;
+			if (treeHeightGrid[currentRow][column] >= currentTreeHeight) {
+				return count;
+			}
+		}
+	}
+	if (direction === Direction.Right) {
+		for (let column = currentColumn + 1; column < numColumns; column++) {
+			count++;
+			if (treeHeightGrid[currentRow][column] >= currentTreeHeight) {
+				return count;
+			}
+		}
+	}
+	if (direction === Direction.Up) {
+		for (let row = currentRow - 1; row >= 0; row--) {
+			count++;
+			if (treeHeightGrid[row][currentColumn] >= currentTreeHeight) {
+				return count;
+			}
+		}
+	}
+	if (direction === Direction.Down) {
+		for (let row = currentRow + 1; row < numRows; row++) {
+			count++;
+			if (treeHeightGrid[row][currentColumn] >= currentTreeHeight) {
+				return count;
+			}
+		}
+	}
+
+	return count;
 }
 
 function isVisible(
@@ -120,7 +172,27 @@ async function puzzleOne() {
 }
 async function puzzleTwo() {
 	const grid: TreeHeightGrid = await parseTreeHeightGrid();
+	console.log('Visibility', getVisibility(grid, 1, 2, Direction.Down));
+
+	let maxVisibilityScore: number = 0;
+
+	for (let row = 0; row < grid.length; row++) {
+		for (let column = 0; column < grid[row].length; column++) {
+			const leftVisiblity = getVisibility(grid, row, column, Direction.Left);
+			const rightVisiblity = getVisibility(grid, row, column, Direction.Right);
+			const upVisiblity = getVisibility(grid, row, column, Direction.Up);
+			const downVisiblity = getVisibility(grid, row, column, Direction.Down);
+
+			const visibilityScore: number =
+				leftVisiblity * rightVisiblity * upVisiblity * downVisiblity;
+
+			if (visibilityScore > maxVisibilityScore) {
+				maxVisibilityScore = visibilityScore;
+			}
+		}
+	}
+	console.log(maxVisibilityScore);
 }
 
-puzzleOne();
+// puzzleOne();
 puzzleTwo();
